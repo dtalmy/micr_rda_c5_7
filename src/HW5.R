50bce205a430a74f627f2aac62562b356a9fcf8d
Hadley Donaldson <hdonald1@vols.utk.edu>
2026-02-18 22:19:47 -0500

    Add files via upload

---
+++ src/HW5.R
@@ -0,0 +1,49 @@
+#HW 5-hadley donaldson
+
+library(tidyverse)
+
+#Q1:
+
+
+students <- read_csv("data/sample_data.csv", 
+                     na = c("N/A", ""))
+
+students |> 
+  rename(
+    student_id = `Student ID`,
+    full_name = `Full Name`, 
+    favourite_food = favourite.food,
+    meal_plan = mealPlan, age = AGE
+  ) |>
+  mutate(
+    meal_plan = factor(meal_plan),
+    age = parse_number(if_else(age == "five", "5", age))
+  )
+
+
+
+#Q2:
+
+
+library(readr)
+colloquium_assessment <- read_csv("data/colloquium_assessment.csv")
+library(dplyr)
+library(tidyr)
+
+colloquium_assessment <- colloquium_assessment |>
+  mutate(across(Q7:Q10, as.numeric))
+
+colloquium_assessment <- colloquium_assessment |> pivot_longer(
+  cols = Q7:Q10,
+  names_to = "question",
+  values_to = "response"
+)
+
+
+#Q3:
+
+question_means <- colloquium_assessment |>
+  group_by(question) |>
+  summarize(mean_score = mean(response, na.rm = TRUE))
+
+
